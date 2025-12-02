138619062d38988255f9329fcf9a06251b6b6b91
Felix Matouschek <fmatouschek@redhat.com>
2025-12-02 14:18:09 +0100

    ci(hack): Enable Snapshot feature gate
    Enable the Snapshot feature gate in kubevirt and kubevirtci providers.
    It is required for testing VirtualMachineTemplateRequests.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- hack/kubevirt.sh
+++ hack/kubevirt.sh
@@ -42,6 +42,9 @@ function kubevirt::up() {
   KUBECONFIG=$(kubevirt::kubeconfig)
   export KUBECONFIG
 
+  echo "enabling snapshot feature gate"
+  ${_kubectl} patch kv/kubevirt -n kubevirt --type merge -p '{"spec": {"configuration": {"developerConfiguration": {"featureGates": ["Snapshot"]}}}}'
+
   echo "waiting for kubevirt to become ready, this can take a few minutes..."
   ${_kubectl} -n kubevirt wait kv kubevirt --for condition=Available --timeout=15m
 }
