cffdf09d74cb355084278d82aa0c9740533f82a7
Felix Matouschek <fmatouschek@redhat.com>
2025-12-08 13:46:48 +0100

    chore(tests): Cleanup imports and use global template client
    Make use of tree local imports consistent across test files and create a
    global template client for testing purposes.
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/template_test.go
+++ tests/template_test.go
@@ -33,31 +33,21 @@ import (
 
 	virtv1 "kubevirt.io/api/core/v1"
 
-	subresourcesv1alpha1 "kubevirt.io/virt-template-api/core/subresourcesv1alpha1"
-	templatev1alpha1 "kubevirt.io/virt-template-api/core/v1alpha1"
-	templateclient "kubevirt.io/virt-template-client-go/virttemplate"
+	"kubevirt.io/virt-template-api/core/subresourcesv1alpha1"
+	"kubevirt.io/virt-template-api/core/v1alpha1"
 )
 
 var _ = Describe("VirtualMachineTemplate", Ordered, func() {
-	var client templateclient.Interface
-
-	BeforeAll(func() {
-		var err error
-
-		client, err = templateclient.NewForConfig(virtClient.Config())
-		Expect(err).NotTo(HaveOccurred())
-	})
-
 	It("should process a VirtualMachineTemplate and return a VirtualMachine", func() {
 		const desiredCPUs = 4
 
-		template := &templatev1alpha1.VirtualMachineTemplate{
+		template := &v1alpha1.VirtualMachineTemplate{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "simple-template",
 				Namespace: NamespaceTest,
 			},
-			Spec: templatev1alpha1.VirtualMachineTemplateSpec{
-				Parameters: []templatev1alpha1.Parameter{
+			Spec: v1alpha1.VirtualMachineTemplateSpec{
+				Parameters: []v1alpha1.Parameter{
 					{
 						Name:  "CPU_COUNT",
 						Value: "2",
@@ -84,26 +74,27 @@ var _ = Describe("VirtualMachineTemplate", Ordered, func() {
 				},
 			},
 		}
-		_, err := client.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).Create(context.Background(), template, metav1.CreateOptions{})
+		_, err := tplClient.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).
+			Create(context.Background(), template, metav1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 
 		// Process with parameter override
 		opts := subresourcesv1alpha1.ProcessOptions{
 			Parameters: map[string]string{"CPU_COUNT": fmt.Sprintf("%d", desiredCPUs)},
 		}
-		processed, err := client.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).Process(context.Background(), template.Name, opts)
+		processed, err := tplClient.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).Process(context.Background(), template.Name, opts)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(processed.VirtualMachine).NotTo(BeNil())
 		Expect(processed.VirtualMachine.Spec.Template.Spec.Domain.CPU.Cores).To(Equal(uint32(desiredCPUs)))
 	})
 
 	It("should create a VirtualMachine from VirtualMachineTemplate", func() {
-		template := &templatev1alpha1.VirtualMachineTemplate{
+		template := &v1alpha1.VirtualMachineTemplate{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "create-template",
 				Namespace: NamespaceTest,
 			},
-			Spec: templatev1alpha1.VirtualMachineTemplateSpec{
+			Spec: v1alpha1.VirtualMachineTemplateSpec{
 				VirtualMachine: &runtime.RawExtension{
 					Object: &virtv1.VirtualMachine{
 						ObjectMeta: metav1.ObjectMeta{
@@ -117,12 +108,13 @@ var _ = Describe("VirtualMachineTemplate", Ordered, func() {
 				},
 			},
 		}
-		_, err := client.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).Create(context.Background(), template, metav1.CreateOptions{})
+		_, err := tplClient.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).
+			Create(context.Background(), template, metav1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 
 		// Create VM from template
 		opts := subresourcesv1alpha1.ProcessOptions{}
-		processed, err := client.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).CreateVirtualMachine(
+		processed, err := tplClient.TemplateV1alpha1().VirtualMachineTemplates(NamespaceTest).CreateVirtualMachine(
 			context.Background(),
 			template.Name,
 			opts)
